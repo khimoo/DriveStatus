8eef1a3223c533a05a2bbea2b155f85f2da57b69
khimoo <dailysentence1111@gmail.com>
2024-05-09 02:07:44 +0900

    update

---
+++ app/status/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Status(models.Model):
+    is_using = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
