4eb422efb308ae5057eb3b2e12f2214067dca2ba
khimoo <dailysentence1111@gmail.com>
2024-05-09 19:22:33 +0900

    update

--- app/drivetracker/settings.py
+++ app/drivetracker/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
+env=environ.Env()
+env.read_env(os.path.join(BASE_DIR,'.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 
 # Application definition
 
