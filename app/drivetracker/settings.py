cf1f0bb2a8f51c561a914208a39a5fb6f3a85899
khimoo <dailysentence1111@gmail.com>
2024-05-09 19:19:36 +0900

    update

--- app/drivetracker/settings.py
+++ app/drivetracker/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&-s&bk+)okcf^d#ux+^5p-@_p%i&jr^e7+unh69r$&u$c)7qfj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
