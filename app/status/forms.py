30a8d20cf909169ba57c57e29af12fde3a72af34
khimoo <dailysentence1111@gmail.com>
2024-08-27 22:44:51 +0900

    update

--- app/status/forms.py
+++ app/status/forms.py
@@ -3,13 +3,24 @@ from django import forms
 
 class ReservationForm(forms.Form):
     name = forms.CharField(
-            label='ユーザー名',
-            max_length=100,
-            widget=forms.TextInput(attrs={'class': 'form-control'})
-            )
+        label='ユーザー名',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
     password = forms.CharField(max_length=100)
     start_date = forms.DateTimeField(
-            label='開始日時',
-            widget=forms.DateTimeInput(attrs={'class': 'form-control'})
-            )
-    usage_time = forms.IntegerField()
+        label='開始日時',
+        widget=forms.DateTimeInput(
+            attrs={'class': 'form-control', 'id': 'start_date',
+                   'placeholder': 'YYYY-MM-DD HH:MM'},
+            format='%Y-%m-%d %H:%M'
+        )
+    )
+    end_date = forms.DateTimeField(
+        label='終了日時',
+        widget=forms.DateTimeInput(
+            attrs={'class': 'form-control', 'id': 'end_date',
+                   'placeholder': 'YYYY-MM-DD HH:MM'},
+            format='%Y-%m-%d %H:%M'
+        )
+    )
